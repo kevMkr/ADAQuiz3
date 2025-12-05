df7e8158e4366e4c9bd57d5d8b7a7ab2a923dfe9
kevMkr <kevinmakmurop@gmail.com>
2025-12-05 08:47:40 +0700

    binarysearhc

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,1 +1,21 @@
+def binarySearch(arr, x):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            low = mid + 1
+            high = mid - 1
+    return -1
 
+if __name__ == '__main__':
+    arr =  [2, 5, 7, 12, 15, 18, 21]
+    x = 7
+
+    result = binarySearch(arr, x)
+    if result != -1:
+        print("Element is present at index", result)
+    else:
+        print("Element is not present in array")
